a86d5e887c4d49f2df6034a9605a4e5e444f2263
fanchang.run <1443023452@qq.com>
2018-01-17 15:04:15 +0800

    可以加载 主页面地址：localhost/

--- common/src/main/java/com/igl/gov/App.java
+++ common/src/main/java/com/igl/gov/App.java
@@ -4,6 +4,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.stereotype.Controller;
 import static org.springframework.boot.SpringApplication.run;
 
+/**
+ * 项目启动主方法类
+ */
 @SpringBootApplication
 @Controller
 public class App {
