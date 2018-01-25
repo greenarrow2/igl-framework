b60a08e90d273c7551023131654e97e5b4178532
fanchang.run <1443023452@qq.com>
2018-01-25 17:23:10 +0800

    Merge branch 'master' of https://gitee.com/MyJavaCode/igl-framework
    # Conflicts:
    #	shiro/igl-framework-shiro.iml

--- system/src/main/java/com/igl/gov/system/service/impl/SysDictionaryServiceImpl.java
+++ system/src/main/java/com/igl/gov/system/service/impl/SysDictionaryServiceImpl.java
@@ -1,13 +1,14 @@
 package com.igl.gov.system.service.impl;
 
-//import com.igl.gov.redis.cache.RedisCache;
-//import com.igl.gov.redis.util.RedisConst;
+import com.igl.gov.redis.cache.RedisCache;
+import com.igl.gov.redis.util.RedisConst;
 import com.igl.gov.system.dao.SysDictionaryDao;
 import com.igl.gov.system.dto.SysDictSimpleDto;
 import com.igl.gov.system.entity.SysDictionary;
 import com.igl.gov.system.service.SysDictionaryService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -18,19 +19,19 @@ public class SysDictionaryServiceImpl  implements SysDictionaryService{
     @Autowired
     private SysDictionaryDao sysDictionaryDao;
 
-//    @Autowired
-//    private RedisCache redisCache;
+    @Autowired
+    private RedisCache redisCache;
 
 
     @Override
     public List<SysDictSimpleDto> querySysDictionaryByDictNo(Integer dictNo) {
-//       List<SysDictSimpleDto> dictionaryDtos = redisCache.getListCache(RedisConst.SYS_DICT + dictNo,SysDictSimpleDto.class );
-//       if(dictionaryDtos == null || dictionaryDtos.size() == 0){
+      List<SysDictSimpleDto> dictionaryDtos = redisCache.getListCache(RedisConst.SYS_DICT + dictNo,SysDictSimpleDto.class );
+       if(dictionaryDtos == null || dictionaryDtos.size() == 0){
            Map<String,Object> param = new HashMap<>(1);
            param.put("dictNo",dictNo);
-        List<SysDictSimpleDto>  dictionaryDtos =  sysDictionaryDao.querySysDictionaryByDictNo(param);
-//           redisCache.putListCache(RedisConst.SYS_DICT + dictNo,dictionaryDtos);
-//       }
+           dictionaryDtos =  sysDictionaryDao.querySysDictionaryByDictNo(param);
+           redisCache.putListCache(RedisConst.SYS_DICT + dictNo,dictionaryDtos);
+       }
         return  dictionaryDtos;
     }
 
