4aa700c673f554c5076ed938e1270d3bb5d2486a
fanchang.run <1443023452@qq.com>
2018-02-07 10:43:49 +0800

    Merge branch 'master' of https://gitee.com/MyJavaCode/igl-framework
    # Conflicts:
    #	shiro/igl-framework-shiro.iml

---
+++ system/src/main/java/com/igl/gov/system/dao/SysRoleDao.java
@@ -0,0 +1,22 @@
+package com.igl.gov.system.dao;
+
+import com.igl.gov.system.dto.SysRoleDto;
+import com.igl.gov.system.entity.SysRole;
+import org.apache.ibatis.annotations.Mapper;
+
+import java.util.List;
+import java.util.Map;
+
+@Mapper
+public interface SysRoleDao {
+
+    int insert(SysRole role);
+
+    int update(SysRole role);
+
+    int delete(Map<String,Object> param);
+
+    List<SysRoleDto> query(Map<String,Object> param);
+
+    int count(Map<String,Object> param);
+}
---
+++ system/src/main/java/com/igl/gov/system/dto/SysRoleDto.java
@@ -0,0 +1,75 @@
+package com.igl.gov.system.dto;
+
+import java.io.Serializable;
+
+public class SysRoleDto  implements Serializable {
+    private Integer id;
+
+    private String roleName;
+
+    private Integer roleType;
+
+    private Integer state;
+
+    private Integer createBy;
+
+    private Integer updateBy;
+
+    private Integer version;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getRoleName() {
+        return roleName;
+    }
+
+    public void setRoleName(String roleName) {
+        this.roleName = roleName;
+    }
+
+    public Integer getRoleType() {
+        return roleType;
+    }
+
+    public void setRoleType(Integer roleType) {
+        this.roleType = roleType;
+    }
+
+    public Integer getState() {
+        return state;
+    }
+
+    public void setState(Integer state) {
+        this.state = state;
+    }
+
+    public Integer getCreateBy() {
+        return createBy;
+    }
+
+    public void setCreateBy(Integer createBy) {
+        this.createBy = createBy;
+    }
+
+    public Integer getUpdateBy() {
+        return updateBy;
+    }
+
+    public void setUpdateBy(Integer updateBy) {
+        this.updateBy = updateBy;
+    }
+
+    public Integer getVersion() {
+        return version;
+    }
+
+    public void setVersion(Integer version) {
+        this.version = version;
+    }
+}
